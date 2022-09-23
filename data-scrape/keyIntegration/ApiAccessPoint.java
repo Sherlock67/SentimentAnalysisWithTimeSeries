70d92cfda18261b50a2bb9a4898c312184b21e39
Sherlock67 <alammushfiqul366@gmail.com>
2022-09-23 11:46:16 +0600

    configuring twitter4j for fetching data

---
+++ data-scrape/keyIntegration/ApiAccessPoint.java
@@ -0,0 +1,25 @@
+package keyIntegration;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+class subscribe{
+    public static void main(String[] args){
+      ConfigurationBuilder cb = new ConfigurationBuilder();
+      cb.setDebugEnabled(true)
+      .setOAuthConsumerKey("1QN7SP4omgsbxA6PCS2gO1ISt")
+      .setOAuthConsumerSecret("mvT82S89Bcfi92N6nuUJtcHbPEwDI3SiilcuzcbBI05RJYYq0V")
+      .setOAuthAccessToken("1228613283134894081-S3KeucIjzRutop0vAEYUfM24aalKNG")
+      .setOAuthAccessTokenSecret("CrxwoEF9VDSvc4FoOgWoKJzYFFrAzIfqImpFoTjoURlIv");
+      TwitterFactory tf = new TwitterFactory(cb.build());
+      Twitter twitter = tf.getInstance();
+      try {
+        twitter.updateStatus("hellow world");
+      } catch (TwitterException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+}
